a9442a0a6ecee910098c09e8360a611b82b8546c
Benjamin <nivoset@gmail.com>
2018-09-02 08:49:20 -0400

    Server added

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express"),
+   path = require("path"),
+   app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use("/", function (req, res) {
+   res.render("pages/index");
+});
+app.use("screen", express.static(path.resolve(_dirname, "screen")));
+app.use("controller", express.static(path.resolve(_dirname, "controller")));
+
+
+app.listen(app.get('port'), function () {
+   console.log("App running on " + app.get('port'));
+});
