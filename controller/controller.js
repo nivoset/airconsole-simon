e8ae9841aa6d4cc5c6d4f1def99410412ceaacb6
Benjamin <nivoset@gmail.com>
2018-09-01 21:34:58 -0400

    More!

--- controller/controller.js
+++ controller/controller.js
@@ -1,9 +1,9 @@
-import Boot from '/screen/states/boot.js';
-import Game from '/screen/states/game.js';
-import Gameover from '/screen/states/gameover.js';
-import Menu from '/screen/states/menu.js';
-import Preloader from '/screen/states/preloader.js';
-import Stately from '/screen/states/stately.js';
+import Boot from '/controller/states/boot.js';
+import Game from '/controller/states/game.js';
+import Gameover from '/controller/states/gameover.js';
+import Menu from '/controller/states/menu.js';
+import Preloader from '/controller/states/preloader.js';
+import Stately from '/controller/states/stately.js';
 
 game.state.add('boot', new Boot());
 game.state.add('game', new Game());
